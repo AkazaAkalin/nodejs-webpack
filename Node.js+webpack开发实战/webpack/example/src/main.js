0fc1ede103123dae3147e00fcd0de97c1cf6fba6
guohua <873889509@qq.com>
2021-05-28 15:13:14 +0800

    webpack.config.js

---
+++ Node.js+webpack开发实战/webpack/example/src/main.js
@@ -0,0 +1,6 @@
+require('./main.css')
+const logo = require('./LOGO.gif')
+document.getElementById('btn').addEventListener('click', function() {
+    alert('hello world')
+})
+document.querySelector('#img').src = logo
