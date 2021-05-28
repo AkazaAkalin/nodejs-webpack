f758ba5461d43b75beab5c4f4e3090f96a15b0cb
guohua <873889509@qq.com>
2021-05-28 15:12:36 +0800

    webpack 配置完成

---
+++ Node.js+webpack开发实战/Vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class='msg' @click="handclick">{{msg}}</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: 'Hello World'
+        }
+    },
+    methods: {
+        handclick: function () {
+            console.log('handclick', this)
+        }
+    }
+}
+</script>
+<style>
+    .msg {
+        color: red
+    }
+</style>
---
+++ Node.js+webpack开发实战/Vue/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const config = {
+    mode: 'production',
+    entry: './src/main',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }, 
+            {
+                test: /\.css$/,
+                use: [process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'vue-style-loader','css-loader','postcss-loader']
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    resolve: {
+        // extendsions: ['.js','.vue', '.json']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            chunks: ['main'],
+            template: 'index.html',
+            filename: 'index.html'
+        }),
+        new VueLoaderPlugin(),
+    ],
+    devtool:'source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, 'build'),
+        port: 8080,
+    }
+}
+if(process.env.NODE_ENV === 'production') {
+    config.plugins.push(new MiniCssExtractPlugin({
+        filename: 'css/[name].css'
+    }))
+}
+module.exports = config
